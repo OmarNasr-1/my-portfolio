e6a2dd77f2e1e9a43df04e150373a0ff6c5ae46b
mohammed <mohammedalysy21.com>
2021-08-12 17:23:46 +0200

    Third Website

---
+++ js/Portfolio.js
@@ -0,0 +1,113 @@
+// Animation On Scroll
+window.addEventListener(`load`,function(){
+    window.addEventListener(`scroll`,function(){
+        var header = document.querySelector(`header`);
+        header.classList.toggle("sticky",window.scrollY > 0 );
+        var lonks = document.querySelector(`ul`);
+        lonks.classList.toggle("lonks-action",window.scrollY > 0 );
+    })
+    AOS.init();
+    // Burger
+const navSlide = () => {
+    const burger = document.querySelector(`.burger`);
+    const links = document.querySelector(`ul`);
+    burger.addEventListener(`click`,()=>{
+        links.classList.toggle(`nav-active`)
+    })
+}
+navSlide();
+
+// Burger Letter X
+const menubtn=document.querySelector(`.burger`);
+let menuOpen = false;
+
+menubtn.addEventListener(`click`,()=>{
+    if(!menuOpen){
+        menubtn.classList.add(`open`);
+        menuOpen=true;
+    }else{
+        menubtn.classList.remove(`open`);
+        menuOpen=false;
+    }
+});
+
+// Skills Animation 
+var Skills={
+    ht:86,
+    js:80,
+    cs:90,
+    jq:75
+};
+$.each(Skills,  function(key,value){
+    var  skillbar=$('.' + key);
+    
+    skillbar.animate(
+    {
+        width:value + "%"
+    },
+    3000,
+    function(){
+        $(".speech-bubble").fadeIn();
+    }
+    );
+});
+})
+
+
+
+
+
+
+
+
+/ Burger
+const navSlide = () => {
+    const burger = document.querySelector(`.burger`);
+    const links = document.querySelector(`ul`);
+    burger.addEventListener(`click`,()=>{
+        links.classList.toggle(`nav-active`)
+    })
+}
+navSlide();
+
+// Burger Letter X
+const menubtn=document.querySelector(`.burger`);
+let menuOpen = false;
+
+menubtn.addEventListener(`click`,()=>{
+    if(!menuOpen){
+        menubtn.classList.add(`open`);
+        menuOpen=true;
+    }else{
+        menubtn.classList.remove(`open`);
+        menuOpen=false;
+    }
+});
+
+// Skills Animation 
+var Skills={
+    ht:85,
+    js:75,
+    cs:90,
+    jq:70
+};
+$.each(Skills,  function(key,value){
+    var  skillbar=$('.' + key);
+    
+    skillbar.animate(
+    {
+        width:value + "%"
+    },
+    3000,
+    function(){
+        $(".speech-bubble").fadeIn();
+    }
+    );
+});
+
+
+
+window.addEventListener(`scroll`,function(){
+    var header = document.querySelector(`header`);
+    header.classList.toggle("sticky",window.scrollY > 0 );
+})
